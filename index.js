cf771e6b912f1089387ece4846ea82d4045a68f5
Ganisher <ganisherbaxtiyarov0732@gmail.com>
2025-11-01 15:40:22 +0500

    d

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 import TelegramBot from "node-telegram-bot-api";
 
-const TOKEN = "8216365587:AAFuVAJTmM-s5w6_KkNAGiXyB4esVc30wqU"
+const TOKEN = "8216365587:AAFuVAJTmM-s5w6_KkNAGiXyB4esVc30wqU"; 
 
-const bot = new TelegramBot(TOKEN, { polling: true })
+const bot = new TelegramBot(TOKEN, { polling: true });
 
-bot.on("message", function (msg) {
+bot.on("message", (msg) => {
     const chatId = msg.chat.id;
-    const text = msg.text;
-    // bot.sendMessage(chatId, `Salom ${text} yaxshimisan ishlar yaxshimi qanday yordam bera olaman?`);
-    console.log(msg);
-    bot.sendMessage(chatId, `Salom  ${firstname} yaxshimisan ishlar yaxshimi qanday yordam bera olaman?`, {
-        reply_markup: {
-            keyboard: [
-                [{ text: "Boshlash" }],
-                [{ text: "Menyu" }, { text: "Sozlamalar" }],
-            ],
-            resize_keyboard: true,
-        },
-    })
+    const firstName = msg.from.first_name; -
 
+    bot.sendMessage(
+        chatId,
+        `Salom ${firstName}! 😊Ishlaring yaxshimi? Qanday yordam bera olaman?`,
+        {
+            reply_markup: {
+                keyboard: [
+                    [{ text: "Boshlash" }],
+                    [{ text: "Menyu" }, { text: "Sozlamalar" }],
+                ],
+                resize_keyboard: true, 
+                one_time_keyboard: false, 
+            },
+        }
+    );
 });
-
